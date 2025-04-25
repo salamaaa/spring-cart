30f4ad65ce0de96d2761770371397b786fc31254
salamaaa <mohamed.salamafciscu@gmail.com>
2025-04-25 20:28:48 +0200

    making ProductServiceImpl implements ProductService interface and implementing all methods within it

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/product/ProductServiceImpl.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/product/ProductServiceImpl.java
@@ -1,4 +1,60 @@
 package com.softpedia.DemoShoppingCart.services.product;
 
-public class ProductServiceImpl {
+import com.softpedia.DemoShoppingCart.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    @Override
+    public Product createProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product findProductById(Long prodId) {
+        return null;
+    }
+
+    @Override
+    public void updateProductById(Product product, Long prodId) {
+
+    }
+
+    @Override
+    public void deleteProductById(Long prodId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
 }
