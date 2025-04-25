a128540f2aaec374bae2195fda6232781892aefa
salamaaa <mohamed.salamafciscu@gmail.com>
2025-04-25 14:14:40 +0200

    Adding the project basic structure

---
+++ demo-shopping-cart/src/test/java/com/softpedia/DemoShoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softpedia.DemoShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
