1b531334899b553998873f03c7f978ae2beb9160
salamaaa <mohamed.salamafciscu@gmail.com>
2025-04-25 20:26:55 +0200

    adding methods signatures related to ProductService interface

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/product/ProductService.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/product/ProductService.java
@@ -1,4 +1,18 @@
 package com.softpedia.DemoShoppingCart.services.product;
 
+import com.softpedia.DemoShoppingCart.models.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    Product createProduct(Product product);
+    Product findProductById(Long prodId);
+    void updateProductById(Product product,Long prodId);
+    void deleteProductById(Long prodId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByCategoryAndBrand(String category,String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand,String name);
+    Long countProductsByBrandAndName(String brand,String name);
 }
