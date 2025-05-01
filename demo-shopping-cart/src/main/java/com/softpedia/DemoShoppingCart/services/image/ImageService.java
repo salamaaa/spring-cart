f252bfaec10451f6adc3c2fb13af2d83b83d0a01
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-01 16:18:17 +0200

    creating ImageNotFoundException + findImageById

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/image/ImageService.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/image/ImageService.java
@@ -1,12 +1,9 @@
 package com.softpedia.DemoShoppingCart.services.image;
 
-import com.softpedia.DemoShoppingCart.dto.ImageDto;
-import com.softpedia.DemoShoppingCart.dto.ProductDto;
 import com.softpedia.DemoShoppingCart.models.Image;
-import com.softpedia.DemoShoppingCart.models.Product;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+
 
 public interface ImageService {
     Image findById(Long id);
--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/image/ImageServiceImpl.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/image/ImageServiceImpl.java
@@ -1,4 +1,37 @@
 package com.softpedia.DemoShoppingCart.services.image;
 
-public class ImageServiceImpl {
+import com.softpedia.DemoShoppingCart.exception.ImageNotFoundException;
+import com.softpedia.DemoShoppingCart.models.Image;
+import com.softpedia.DemoShoppingCart.repos.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService{
+
+    private final ImageRepository imageRepository;
+
+    @Override
+    public Image findById(Long id) {
+        return imageRepository.findById(id).orElseThrow(
+                ()-> new ImageNotFoundException("Image not Found!")
+        );
+    }
+
+    @Override
+    public Image saveImage(MultipartFile file, Long productId) {
+        return null;
+    }
+
+    @Override
+    public void updateImageById(MultipartFile multipartFile, Long imageId) {
+
+    }
+
+    @Override
+    public void deleteImage(Long id) {
+
+    }
 }
