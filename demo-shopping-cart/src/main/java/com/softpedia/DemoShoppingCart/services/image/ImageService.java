d3baf61f968e1be2f9fd1a7de639c1593f008ff5
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-01 16:12:53 +0200

    refactoring ImageService

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/image/ImageService.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/image/ImageService.java
@@ -4,15 +4,13 @@ import com.softpedia.DemoShoppingCart.dto.ImageDto;
 import com.softpedia.DemoShoppingCart.dto.ProductDto;
 import com.softpedia.DemoShoppingCart.models.Image;
 import com.softpedia.DemoShoppingCart.models.Product;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ImageService {
     Image findById(Long id);
-    Image addImage(ImageDto imageDto);
-    Image CreateImage(ImageDto imageDto, Product product);
-    Image updateImageById(ImageDto imageDto,Long id);
-    Image updateExistingImage(Image existingImage, ImageDto imageDto);
+    Image saveImage(MultipartFile file, Long productId);
+    void updateImageById(MultipartFile multipartFile,Long imageId);
     void deleteImage(Long id);
-    List<Image> getAllImages();
 }
