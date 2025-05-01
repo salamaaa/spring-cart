27317b0fb18a06c24f98a8a10e39c1ba0512ce15
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-01 14:24:15 +0200

    Creating CategoryService crud methods

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/category/CategoryService.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/services/category/CategoryService.java
@@ -1,4 +1,14 @@
 package com.softpedia.DemoShoppingCart.services.category;
 
+import com.softpedia.DemoShoppingCart.models.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    Category findById(Long id);
+    Category findByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category,Long id);
+    void deleteById(Long id);
 }
