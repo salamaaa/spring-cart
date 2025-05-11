331c5241da6b348146ff15e09d0b288ba8aceaf2
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-11 11:31:47 +0200

    implementing ApiResponse class to get show response data to the frontend

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/response/ApiResponse.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/response/ApiResponse.java
@@ -1,4 +1,11 @@
 package com.softpedia.DemoShoppingCart.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class ApiResponse {
+    private String message;
+    private Object data;
 }
