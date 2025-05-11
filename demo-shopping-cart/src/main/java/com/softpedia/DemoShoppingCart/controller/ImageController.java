65aaf5618811012123a976250a13ae2b4c789404
salamaaa <mohamed.salamafciscu@gmail.com>
2025-05-11 11:51:51 +0200

    implementing saveImages in the ImageController

--- demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/controller/ImageController.java
+++ demo-shopping-cart/src/main/java/com/softpedia/DemoShoppingCart/controller/ImageController.java
@@ -1,4 +1,35 @@
 package com.softpedia.DemoShoppingCart.controller;
 
+import com.softpedia.DemoShoppingCart.dto.ImageDto;
+import com.softpedia.DemoShoppingCart.response.ApiResponse;
+import com.softpedia.DemoShoppingCart.services.image.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/images")
 public class ImageController {
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long productId){
+        try {
+            List<ImageDto> imageDtos = imageService.saveImages(files,productId);
+            return ResponseEntity.ok(new ApiResponse("Upload Success",imageDtos));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload Failed",e.getMessage()));
+        }
+    }
+
 }
